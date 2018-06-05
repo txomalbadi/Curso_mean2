a63ef8964771a68ee2ddd9eea902337f46bbf3d0
Domingo Malvadi <roberto.domingo@atos.net>
2018-06-05 16:18:02 +0200

    'estable1'

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,11 @@
 en request es lo que va a recibir en la petición y las response 
 el lo que va a devolver
 */
+// cargamos bcrypt para encriptacion de datos, en este caso la contraseña de usuario
+var bcrypt = require('bcrypt-nodejs');
+//importamos el modelo para la funcion de incorporar nuevos usurios SaveUser
+// con require s e carga el modelo  o lo que se necesite
+var Usumodelo = require('../models/Usuario');
 
 function pruebas(req, res){
     res.status(200).send({
@@ -11,6 +16,31 @@ function pruebas(req, res){
 
 };
 
+function salvaUsuario(req, res){
+//instancio un nuevo usuario del moelo usuario
+    var user = new Usumodelo();
+//en params almaceno los datos del push dónde se habá solicitado los datos en el combo
+    var params = req.body;
+
+    console.log(params);
+    
+    user.nombre = params.nombre;
+    user.apellido = params.apellido;
+    user.email = params.email;
+    user.pasww = params.pasww;
+    user.perfil = 'ROLE_USER';
+    user.imagen = 'null';
+
+    if (params.pasww) {
+    // encriptar la contraseña y guardar datos
+    } else {
+        res.status(500).send({message: 'Introducir la contraseña'});
+    }
+
+
+}
+
 module.exports = {
-    pruebas
+    pruebas,
+    salvaUsuario
 };
--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,6 @@ var UserControler = require('../controllers/user');
 var api = express.Router();
 
 api.get('/probando-controlador', UserControler.pruebas);
-
+api.post('/registro-usuario', UserControler.salvaUsuario);
 module.exports = api;
 
